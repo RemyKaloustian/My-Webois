fbc49372a3f3087db58e3a2ab944a97a31b07ced
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-01-17 10:45:45 +0100

    Adding the Google map :clap: :clap:

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,15 +1,35 @@
 <template>
-  
-</template>
+<gmap-map
+    :center="center"
+    :zoom="7"
+    style="width: 500px; height: 300px"
+  >
+    <gmap-marker
+      :key="index"
+      v-for="(m, index) in markers"
+      :position="m.position"
+      :clickable="true"
+      :draggable="true"
+      @click="center=m.position"
+    ></gmap-marker>
+  </gmap-map></template>
 
 <script>
-export default {
+  
+ 
+  export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to the webois'
+      msg: 'Welcome to the webois',
+      center: {lat: 10.0, lng: 10.0},
+        markers: [{
+          position: {lat: 10.0, lng: 10.0}
+        }, {
+          position: {lat: 11.0, lng: 11.0}
+        }]
     }
-  }
+  } 
 }
 </script>
 
