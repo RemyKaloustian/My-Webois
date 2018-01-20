e8beb812aa28c904b457c8d5044977dc270b7fc3
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-01-20 19:07:21 +0100

    LIGHTWEIGHT BABY !!!!!! :boom: (and also mega bug of the death)

--- client/src/components/Map.vue
+++ client/src/components/Map.vue
@@ -5,34 +5,15 @@
   </div>
 </template>
 <script>
+
+//J'ai testé des trucs en global mais ça fait pareil on dirait
+let _currentLoc = {};
+_currentLoc.tamaire = "kk";
+
 export default {
   name: 'google-map',
   props: ['name'],
-  methods:{   
-
-    centerMapToCurrentLocation(){
-      navigator.geolocation.getCurrentPosition((position) => {
-        var currentLocation = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-        console.log(currentLocation);
-        this.map.panTo(new google.maps.LatLng(currentLocation.lat, currentLocation.lng));
-        this.map.setZoom(20);
-        this.addMarker(currentLocation);
-        //Watch position, pour la mise à jour de la position du gars
-      });      
-    },
-
-    addMarker(LatLngObj){
-      var marker =  new google.maps.Marker({
-        position: new google.maps.LatLng(LatLngObj.lat, LatLngObj.lng),
-        map:this.map,
-        icon:'http://www.magic-emoji.com/emoji/images/619_emoji_iphone_ok_hand_sign.png'
-      });
-    }
-  },
-  data: function () {
+   data: function () {
     return {
       mapName: this.name + "-map",
       markerCoordinates: [{
@@ -47,9 +28,63 @@ export default {
       }],
       map: null,
       bounds: null,
-      markers: []
+      markers: [], 
+      baybay:"tt",//pour tester
+      currentLocation:{} //C'est ça qu'on veut modifier
     }
   },
+  methods:{ 
+    updateCurrentPosition(){    
+      navigator.geolocation.getCurrentPosition((position) => {
+        //Là on set ce putain d'objet
+        this.currentLocation = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        console.log("After setting, This.currentLocation = ");
+        console.log(this.currentLocation);//Là ça affiche bien l'objet
+
+        this.tamaire = "tété";
+       _currentLoc.lat = this.currentLocation.lat;
+        _currentLoc.lng = this.currentLocation.lng;
+        console.log(_currentLoc.lat); //Là aussi les affichages marchent
+        console.log(_currentLoc.lng);
+
+        this.baybay = "AYNORMHEU";
+        console.log(this.baybay);//Là aussi ça marche
+        this.$set(this, 'baybay', "AYNORMHEU"); //ça c'est sur le site de VUE         
+
+        this.map.panTo(new google.maps.LatLng(this.currentLocation.lat, this.currentLocation.lng));
+        this.map.setZoom(18);
+      });      
+    },  
+
+    initializeMap(){        
+      this.updateCurrentPosition(); 
+
+      console.log("Before addMarker, This.currentLocation = ");
+      console.log(this.currentLocation); //Là on a plus les valeurs qu'on a set,, objet vide
+      console.log(_currentLoc.lat);//marche pas
+      console.log(_currentLoc.lng);//marche pas 
+      console.log(_currentLoc.tamaire);//pas changé alors que modifiée dans updateCurrentPosition
+      console.log(this.baybay); //Là la valeur est pas changée(alors qu'on a mis AYNORMEUH avant)
+        
+      this.addMarker(this.currentLocation);
+    },
+
+    addMarker(LatLngObj){
+      //on dirait que LatLngObj marche pas non plus
+      console.log("In addMarker This.currentLocation = ");
+      console.log(this.currentLocation);  //marche pas objet vide
+        console.log(this.baybay);//same shit     
+        
+      var marker =  new google.maps.Marker({
+        position: new google.maps.LatLng(LatLngObj.lat, LatLngObj.lng),
+        map:this.map,
+        icon:'http://www.magic-emoji.com/emoji/images/619_emoji_iphone_ok_hand_sign.png'
+      });
+    }
+  }, 
   mounted: function () {
     this.bounds = new google.maps.LatLngBounds();
     const element = document.getElementById(this.mapName)
@@ -62,16 +97,13 @@ export default {
       const position = new google.maps.LatLng(coord.latitude, coord.longitude);
       const marker = new google.maps.Marker({ 
         position,
-        map: this.map
-        
+        map: this.map        
       });
     this.markers.push(marker)
       this.map.fitBounds(this.bounds.extend(position))
     });
-    this.centerMapToCurrentLocation();
-    
-  }
- 
+    this.initializeMap();    
+  } 
 };
 </script>
 <style scoped>
