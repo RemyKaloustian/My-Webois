9fe89f3116903493fdd563659e71038696ad26c1
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-01-22 15:47:25 +0100

    Adding a map component :boom: :clap: :fist:

---
+++ client/webois/src/Map.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+class Map extends React.Component
+{
+    render()
+    {
+        return(
+            <h1> Yo là c'est la map normalement</h1>
+        );
+    }
+}
+
+export default Map;
