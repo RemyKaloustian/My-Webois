035b3b39388d0d295d03f37d8d50eab29cc5d1ad
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-01-29 13:45:03 +0100

    Moving files in client

---
+++ client/src/GoogleMapsWrapper.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { GoogleMap,withGoogleMap,withScriptjs } from 'react-google-maps';
+
+ const GoogleMapsWrapper = withScriptjs(withGoogleMap(props => {
+console.log(props.onMapMounted);
+     
+  return <GoogleMap {...props} ref={props.onMapMounted}>{props.children}</GoogleMap>
+}));
+
+export default GoogleMapsWrapper;
