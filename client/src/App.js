de36c05648c3def53af1009eb98e215562dc016a
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-02-01 10:11:09 +0100

    Adding menu to page

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MapPage from './MapPage'
+import Menu from './Menu';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        <MapPage />
+       <Menu />
       </div>
     );
   }
