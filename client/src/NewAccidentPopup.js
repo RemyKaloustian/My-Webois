8ea050bb72930985ec4e6fe12081112b14b0f846
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-02-01 11:57:51 +0100

    Hiding accident popup from start

--- client/src/NewAccidentPopup.js
+++ client/src/NewAccidentPopup.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 
 
 class NewAccidentPopup extends Component
 {
+    componentDidMount(){
+        $('#accident-popup').hide();
+
+    }
 
     render(){
-        return (<div>New accident popup</div>);
+        return (<div id="accident-popup">New accident popup</div>);
     }
 
 
