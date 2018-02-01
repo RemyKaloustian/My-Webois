9d5ae4c8df48dcb6222214b2eda3f284afa7ee32
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-02-01 10:54:34 +0100

    Hiding ManagerView on mount

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import MapPage from './MapPage'
 import Menu from './Menu';
+import ManagerView from './ManagerView';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
        <MapPage ref="mapComponent"/>
        <Menu onManagerViewClick = {() => this.refs.mapComponent.hide()}/>
+       <ManagerView/>
       </div>
     );
   }
---
+++ client/src/ManagerView.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import $ from "jquery";
+
+class ManagerView extends Component
+{
+
+    componentDidMount() 
+    {
+        $('#manager-view').hide();
+    }
+
+    render() {        
+        return (
+          <div id="manager-view" >
+                The manager view
+          </div>
+        );
+      }
+}
+
+export default ManagerView;
