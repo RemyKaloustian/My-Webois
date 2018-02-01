7aa7bfbe07ae33187dea1aa792a93d77f0f74402
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-02-01 17:14:54 +0100

    Basic style for new accident popup

--- client/src/NewAccidentPopup.js
+++ client/src/NewAccidentPopup.js
@@ -6,6 +6,8 @@ class NewAccidentPopup extends Component
 {
     componentDidMount(){
         $('#accident-popup').hide();
+        //Centering the popup in here cuz it breaks my balls to do it in css
+        $('#accident-popup').css('margin-left', $(window).width()/2 - $('#accident-popup').width()/2);
 
     }
 
@@ -17,6 +19,7 @@ class NewAccidentPopup extends Component
     render(){
         return (
             <div id="accident-popup">
+            <button>Close</button>
                 <h4>New accident</h4> 
                 <p>Do you really want to signal an accident at your position ?</p>
                 <button>Validate</button>                
