92c2e9cb424781020ce2367a0a081cfaca211fe8
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-02-04 11:59:48 +0100

    Selecting (mocked)

--- client/src/DataBaseSelector.js
+++ client/src/DataBaseSelector.js
@@ -5,6 +5,12 @@ import $ from "jquery";
 class DataBaseSelector extends Component
 {
 
+    selectAccidents = ()=>
+    {
+        //Normally use db calls
+        return ['En France, c\'est plus de 6 virgule 5 millions', 'de tonnes de nourrrrriture ', 'qui sont jetées chaque' ,'année, par les particluiers (connards de particluiers)'];
+    }
+
     //Nothing in render cuz we just want the methods of the database
     render(){ return(<p></p>);}
 }
