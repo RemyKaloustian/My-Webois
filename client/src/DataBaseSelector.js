febc3c1ae68bc896a162468c0346778ff3fe2e89
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-02-04 10:29:22 +0100

    Adding db selector

---
+++ client/src/DataBaseSelector.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import $ from "jquery";
+
+//Is used for selection in the database
+class DataBaseSelector extends Component
+{
+
+    //Nothing in render cuz we just want the methods of the database
+    render(){ return(<p></p>);}
+}
+
+export default DataBaseSelector;
