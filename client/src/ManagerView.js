aaf510bb655e76ca427af71854636ac7bdc4f486
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-02-04 12:00:02 +0100

    Updating the accidents

--- client/src/ManagerView.js
+++ client/src/ManagerView.js
@@ -3,6 +3,7 @@ import $ from "jquery";
 
 class ManagerView extends Component
 {
+    state = {accidents:[]};
 
     componentDidMount() 
     {
@@ -19,10 +20,29 @@ class ManagerView extends Component
         $('#manager-view').hide();
     }
 
+    fill = (results)=>
+    {
+        let resaccidents = [];
+        for (let index = 0; index < results.length; index++) 
+        {
+            resaccidents.push(results[index]);            
+        }
+        this.setState({accidents:resaccidents});
+        console.log(this.state.accidents);
+    }
+
     render() {        
         return (
           <div id="manager-view" >
                 The manager view
+                <div>
+                {
+                    this.state.accidents.map(function(item, i)
+                    {
+                        return (<p key={i}>{item}</p>);
+                    })
+                }
+            </div>
           </div>
         );
       }
