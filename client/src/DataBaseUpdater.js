44020f72e6d5cd1c27353c76350d6263cf0b5052
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-02-04 10:29:36 +0100

    Adding updater :boom:

---
+++ client/src/DataBaseUpdater.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import $ from "jquery";
+
+//Is used for updates on the DB such as insertion/deletion/modification
+class DataBaseUpdater extends Component
+{
+
+    //Nothing in render cuz we just want the methods of the database
+    render(){return(<p></p>);}
+}
+
+export default DataBaseUpdater;
