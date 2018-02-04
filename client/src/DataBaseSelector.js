f42ac1b1061337f3cc6af1fe026a0a99a8061057
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-02-04 14:20:08 +0100

    Getting results from accidents position

--- client/src/DataBaseSelector.js
+++ client/src/DataBaseSelector.js
@@ -5,10 +5,17 @@ import $ from "jquery";
 class DataBaseSelector extends Component
 {
 
+    //This one is used for the manager view
     selectAccidents = ()=>
     {
         //Normally use db calls
         return ['En France, c\'est plus de 6 virgule 5 millions', 'de tonnes de nourrrrriture ', 'qui sont jetées chaque' ,'année, par les particluiers (connards de particluiers)'];
+    }
+
+    //This one is used for the map view
+    selectAccidentsPosition = ()=>
+    {
+        return [{id:1, latitude:43.616650 , longitude: 7.075074}, {id:2, latitude:43.617551 , longitude: 7.068636}, {id:3, latitude:43.614786 , longitude: 7.067993}];
     }
 
     //Nothing in render cuz we just want the methods of the database
