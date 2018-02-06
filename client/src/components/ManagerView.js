3531a4e7553467d92f2ff1cd2cfc2aff66e6fa15
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-02-06 15:42:58 +0100

    Super stylish animation for manager view

--- client/src/components/ManagerView.js
+++ client/src/components/ManagerView.js
@@ -9,18 +9,29 @@ class ManagerView extends Component
 
     componentDidMount() 
     {
-        $('#manager-view').hide();
+        //$('#manager-view').hide();
         $('#manager-view').css('max-height', $(window).height() - $('#bottom-menu').height());
     }
 
     show = () =>
     {
-        $('#manager-view').show();
+        //$('#manager-view').show();
+        //$('#manager-view').addClass('animated-show-managerview');
+       // $('#manager-view').css('margin-left', '0px');
+        $("#manager-view").animate({
+            marginLeft: '0px'
+        }, 500);
+
     }
 
     hide = () =>
     {
-        $('#manager-view').hide();
+        //$('#manager-view').hide();
+        //$('#manager-view').css('margin-left', '100%');
+        $("#manager-view").animate({
+            marginLeft: '100%'
+        }, 500);
+        
     }
 
     fill = (results)=>
