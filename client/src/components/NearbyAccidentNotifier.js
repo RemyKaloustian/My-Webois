8fe3e1f74e0491c989b90ac24e443dd12b3f7c95
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-02-10 09:34:55 +0100

    Showing nearby accident with neat ass animation

--- client/src/components/NearbyAccidentNotifier.js
+++ client/src/components/NearbyAccidentNotifier.js
@@ -4,16 +4,35 @@ import $ from 'jquery';
 
 class NearbyAccidentNotifier extends Component
 {
+    componentDidMount()
+    {
+        $('#nearby-accident-popup').css('width', $(window).width());
+        $('#nearby-accident-popup').css('height', $(window).height());
+        $('#nearby-accident-popup').css('top', '-'+ $(window).height() + 'px');
+        
+    }
+
     //The accidents are passed, in markers form (??? , for right now, yes, marker form)
-    checkNearbyAccidents = (accidentsList) =>
+    checkNearbyAccidents = (curentPosition, accidentsList) =>
     {
 
+    }
+
+    showNearbyAccidentPopup = () =>
+    {
+       
+        $('#nearby-accident-popup').show();
+        $('#nearby-accident-popup').animate({
+            top: '0'
+        }, 500);
+        
     }
 
     notifyNearByAccident = () =>
     {
         console.log("Playing notifier sound");
         $('#notifier-sound')[0].play();
+        this.showNearbyAccidentPopup();
     }
 
 
