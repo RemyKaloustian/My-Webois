28d8380a275195a3b05dc46a8af5299590971cc7
Mathias Chevalier <mc.mumrau@gmail.com>
2018-02-19 09:50:11 +0100

    User schema for login engine

---
+++ server/models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//= ===============================
+// User Schema for login engine
+//= ===============================
+
+const UserSchema = new Schema({
+    username: {
+        type: String
+    },
+    password: {
+        type: String
+    }
+});
+
+module.exports = mongoose.mode('User', UserSchema)
+
