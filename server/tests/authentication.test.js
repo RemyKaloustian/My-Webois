6f4a0c643f4bb1fc64ac34c9cc283a3431eb02df
Mathias Chevalier <mc.mumrau@gmail.com>
2018-02-20 14:14:46 +0100

    Register test

--- server/tests/authentication.test.js
+++ server/tests/authentication.test.js
@@ -10,9 +10,45 @@ const chai = require('chai'),
 
 chai.use(chaiHttp);
 
-const server = null;
+let server = null;
+
+const u = {
+    username: 'userTest',
+    password: 'whatever'
+};
 
 getApiPath = function () {
   return config.apiPath;
 };
 
+describe('Authentication', () => {
+  /* TEST CONTEXT MANAGEMENT */
+  before((done) => {
+    server = chai.request(index);
+    // Emptying DB for collision avoidance
+    User.remove({}).then(() => done());
+  });
+
+  // Emptying DB once done
+  after((done) => {
+    User.remove({}).then(() => done());
+  });
+
+  /* REAL TESTING */
+  describe('Register and login', () => {
+
+    it('Registering userTest', (done) => {
+      server.post(getApiPath() + '/auth/register')
+          .send(u)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.have.property('username').eql(u.username);
+            res.body.should.have.property('password').eql(u.password);
+            res.body.should.have.property('_id');
+            done();
+          });
+    });
+
+
+  });
+});
